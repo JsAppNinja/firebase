771efd65cae6d0997a746c8666c00e8569e056f7
Jeremy Diaz <jeremy@knodemy.com>
2019-06-06 11:24:36 -0700

    Added login UI

---
+++ authdashboard/client/src/App.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import './App.css';
+import 'antd/dist/antd.css';
+
+// Custom Components
+import Login from './components/Login';
+
+function App() {
+  return (
+    <div className="App">
+      <Login />
+    </div>
+  );
+}
+
+export default App;
