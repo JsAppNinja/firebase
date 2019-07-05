cf3da6e8fdbf156d87fc678226370c97f5d6564a
Nazar Pankiv (official name) <info@ols-med.net>
2019-07-05 11:36:25 -0700

    802.updatings5LH

--- authdashboard/client/src/components/dashboard/OLSStats.js
+++ authdashboard/client/src/components/dashboard/OLSStats.js
@@ -4,8 +4,21 @@ export default class Profile extends Component {
  
   render() {
     return (
-      <h1>OLS Stats <iframe src="https://docs.google.com/spreadsheets/d/e/2PACX-1vTgGeSW3UTvQORYelzktd4YxUDcce8QEKxqrBS1VGhzcdI_pHntQ3UVVII4hJ_zG8zBhCOFnWDTKAIs/pubhtml?gid=334324719&amp;single=true&amp;widget=true&amp;headers=false"></iframe></h1>
-   
+      <div>
+      <h1>OLS Stats</h1> 
+      <p> <iframe src="https://docs.google.com/spreadsheets/d/e/2PACX-1vTgGeSW3UTvQORYelzktd4YxUDcce8QEKxqrBS1VGhzcdI_pHntQ3UVVII4hJ_zG8zBhCOFnWDTKAIs/pubhtml?gid=334324719&amp;single=true&amp;widget=true&amp;headers=false"></iframe> </p>
+      
+     
+         <body> 
+         <p> <br></br> </p>
+         </body>
+         <body> 
+         <p> <br></br> </p>
+         </body>
+    
+      <h1>OLS locations</h1> 
+      <p> <iframe src="https://www.google.com/maps/d/u/0/embed?mid=1IeLInyV7cTtRSFS6fZj-VrbsQkA" width="640" height="480"></iframe> </p>
+      </div>
       );
   }
 }
