d73064cb9e3313d0dd9caad94333707cdce5c6b7
Nazar Pankiv (official name) <info@ols-med.net>
2019-07-07 12:42:48 -0700

    804.ols.updates5LH.olssregister

---
+++ authdashboard/client/build/precache-manifest.c92e5c2abec2c75b5cc79f88c24fb091.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "49c950e924624c66f177f63e0e98b6a4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "96281f34fc5f770e5042",
+    "url": "/static/css/2.33bbdc9a.chunk.css"
+  },
+  {
+    "revision": "a6352440d1a196346684",
+    "url": "/static/css/main.8d3f4923.chunk.css"
+  },
+  {
+    "revision": "96281f34fc5f770e5042",
+    "url": "/static/js/2.9d1d6b50.chunk.js"
+  },
+  {
+    "revision": "a6352440d1a196346684",
+    "url": "/static/js/main.b299af2d.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  }
+]);
